1753669cd04ba91f70d86902465b47ce1ea5b0b3
Yonatan Gottesman <yonigo10@gmail.com>
2019-07-27 19:44:08 +0300

    better structure

--- pong.py
+++ pong.py
@@ -1,7 +1,7 @@
 import random
-import sys
 
 import pygame
+
 
 class Paddle(pygame.Rect):
     def __init__(self, velocity, up_key, down_key, *args, **kwargs):
@@ -50,12 +50,13 @@ class Pong:
     COLOUR = (255, 255, 255)
 
     def __init__(self):
+        self.paddles = []
         pygame.init()  # Start the pygame instance.
         self.central_line = pygame.Rect(self.WIDTH / 2, 0, 1, self.HEIGHT)
         # Setup the screen
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock = pygame.time.Clock()
-        
+
         self.init_objects()
 
         self.score_a = 0
@@ -63,9 +64,8 @@ class Pong:
 
     def init_objects(self):
         # Create the player objects.
-        self.paddles = []
-        self.balls = []
-        self.paddles.append(Paddle(  # The left paddle
+
+        self.paddle_a = Paddle(  # The left paddle
             self.VELOCITY,
             pygame.K_w,
             pygame.K_s,
@@ -73,9 +73,9 @@ class Pong:
             self.HEIGHT / 2 - self.PADDLE_HEIGHT / 2,
             self.PADDLE_WIDTH,
             self.PADDLE_HEIGHT
-        ))
+        )
 
-        self.paddles.append(Paddle(  # The right paddle
+        self.paddle_b = Paddle(  # The right paddle
             self.VELOCITY,
             pygame.K_UP,
             pygame.K_DOWN,
@@ -83,40 +83,38 @@ class Pong:
             self.HEIGHT / 2 - self.PADDLE_HEIGHT / 2,
             self.PADDLE_WIDTH,
             self.PADDLE_HEIGHT
-        ))
-
-        self.balls.append(Ball(
+        )
+        self.paddles.append(self.paddle_a)
+        self.paddles.append(self.paddle_b)
+        
+        self.ball = Ball(
             self.VELOCITY,
             self.WIDTH / 2 - self.BALL_WIDTH / 2,
             self.HEIGHT / 2 - self.BALL_WIDTH / 2,
             self.BALL_WIDTH,
             self.BALL_WIDTH
-        ))
-
+        )
 
     def update_losers(self):
-        for ball in self.balls:
-            if ball.x > self.WIDTH:
-                self.score_a = self.score_a + 1
-                return True
-            if ball.x < 0:
-                self.score_b = self.score_b + 1
-                return True
+
+        if self.ball.x > self.WIDTH:
+            self.score_a = self.score_a + 1
+            return True
+        if self.ball.x < 0:
+            self.score_b = self.score_b + 1
+            return True
         return False
 
-
     def check_ball_hits_wall(self):
-        for ball in self.balls:
-            if ball.y > self.HEIGHT - self.BALL_WIDTH or ball.y < 0:
-                ball.angle = -ball.angle
+        if self.ball.y > self.HEIGHT - self.BALL_WIDTH or self.ball.y < 0:
+            self.ball.angle = -self.ball.angle
 
     def check_ball_hits_paddle(self):
-        for ball in self.balls:
-            for paddle in self.paddles:
-                if ball.colliderect(paddle):
-                    ball.velocity = -ball.velocity
-                    ball.angle = random.randint(-10, 10)
-                    break
+        for paddle in self.paddles:
+            if self.ball.colliderect(paddle):
+                self.ball.velocity = -self.ball.velocity
+                self.ball.angle = random.randint(-10, 10)
+                break
 
     def game_loop(self):
         while True:
@@ -128,28 +126,30 @@ class Pong:
             if self.update_losers():
                 self.init_objects()
                 continue
-                
+
             self.check_ball_hits_paddle()
             self.check_ball_hits_wall()
 
             # Redraw the screen.
             self.screen.fill((0, 0, 0))
 
-            for paddle in self.paddles:
-                paddle.move_paddle(self.HEIGHT)
-                pygame.draw.rect(self.screen, self.COLOUR, paddle)
+            self.paddle_a.move_paddle(self.HEIGHT)
+            pygame.draw.rect(self.screen, self.COLOUR, self.paddle_a)
 
-            for ball in self.balls:
-                ball.move_ball()
-                pygame.draw.rect(self.screen, self.COLOUR, ball)
+            self.paddle_b.move_paddle(self.HEIGHT)
+            pygame.draw.rect(self.screen, self.COLOUR, self.paddle_b)
+
+
+            self.ball.move_ball()
+            pygame.draw.rect(self.screen, self.COLOUR, self.ball)
             pygame.draw.rect(self.screen, self.COLOUR, self.central_line)
 
             # Display scores:
             font = pygame.font.Font(None, 74)
             text = font.render(str(self.score_a), 1, self.WHITE)
-            self.screen.blit(text, (self.WIDTH/4, 10))
+            self.screen.blit(text, (self.WIDTH / 4, 10))
             text = font.render(str(self.score_b), 1, self.WHITE)
-            self.screen.blit(text, (self.WIDTH/4*3, 10))
+            self.screen.blit(text, (self.WIDTH / 4 * 3, 10))
 
             pygame.display.flip()
             self.clock.tick(60)
